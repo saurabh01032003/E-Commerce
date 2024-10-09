b80998fe86cd7005c3968da3c80d8ecedeebd2cb
saurabh01032003 <saurabh858182@gmail.com>
2024-10-09 18:54:52 +0530

    change

--- app.js
+++ app.js
@@ -85,6 +85,6 @@ app.get('/',(req,res)=>{
     res.render('home');
 })
 
-app.listen(8080,()=>{
-    console.log("Server Connected at port 8080");
+app.listen(5050,()=>{
+    console.log("Server Connected at port 5050");
 })
